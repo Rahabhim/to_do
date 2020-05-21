67674db32ec0a958179ac4faf72cc3c6c80d96c1
Max <maxiolaz@gmail.com>
2020-05-21 01:12:33 -0300

    Now it adds info to the page

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -9,6 +10,14 @@ def index(request):
 
     tasks = Task.objects.all()
 
-    context = {"tasks":tasks}
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {"tasks":tasks, "form":form}
 
     return render(request, "tasks/list.html", context)
